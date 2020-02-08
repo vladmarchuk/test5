892a8b67080724eb11a38fe858c396c2c04221e4
Vladislav Marchuk <vlad@vladmarchuk.com>
2020-02-09 00:18:13 +0200

    test

--- src/index.js
+++ src/index.js
@@ -6,4 +6,5 @@ document.getElementById("app").innerHTML = `
   We use Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
+changes 1
 `;
